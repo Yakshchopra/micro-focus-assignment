34d89476c516a17539ecd7f751ba5bdf1eb9fb52
Yakshchopra <yakshchopra@gmail.com>
2022-06-07 00:02:35 +0530

    ✨ basic drag implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../components/shared/Footer';
 import Header from '../components/shared/Header';
+import Twiggle from '../components/Twiggle';
+import { useRef } from 'react';
+import { motion } from 'framer-motion';
 
 const Home = () => {
+  const constraintsRef = useRef(null);
+
   return (
-    <div className='bg-light h-screen flex flex-col w-screen p-5'>
+    <div className='bg-light h-screen flex flex-col w-screen p-5 overflow-hidden'>
       {/* Header */}
       <Header />
       {/* main container */}
-      <div className='main h-full'></div>
+      <div
+        className='main h-full flex items-center justify-center relative min-h-400'
+        ref={constraintsRef}
+      >
+        <motion.div
+          drag
+          dragConstraints={constraintsRef}
+          dragSnapToOrigin={true}
+          className='h-64 w-96 bg-custom-blue opacity-25 rounded-xl absolute'
+        ></motion.div>
+      </div>
       <Footer />
       {/* footer */}
     </div>
