a3c9d44611102012bd519ace88cfe09ad37a7e2a
Yakshchopra <yakshchopra@gmail.com>
2022-06-06 23:23:59 +0530

    💄 selector-UI

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Selector from './Selector';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='p-5 flex justify-between bg-white rounded-xl'>
+      {/* selector */}
+      <Selector />
+      {/* description text */}
+      <span>Press escape to hide the window</span>
+      {/* timer */}
+    </header>
+  );
 };
 
 export default Header;
